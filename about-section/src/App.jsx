139f73c58e148818c3b95115ec7e47ae2cb310d1
IntuzPramitK <pramitk@intuz.net>
2025-08-01 10:58:26 +0530

    AAW-19 testing01082025 [auto-pr,development,taskcompleted]

--- about-section/src/App.jsx
+++ about-section/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
             
             <div className="feature">
               <h3>Our Values</h3>
-              <p>Innovation, Integrity, and Excellence in everything we 1 2 </p>
+              <p>Innovation, Integrity, and Excellence in everything we 133 2 </p>
             </div>
           </div>
         </section>
