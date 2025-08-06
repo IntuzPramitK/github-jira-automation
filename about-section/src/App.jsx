e566a937427fec86848b25c4503c5fa10a2d27ce
IntuzPramitK <pramitk@intuz.net>
2025-08-06 14:35:38 +0530

    AAW-19 test [auto-pr,development,taskcompleted]

--- about-section/src/App.jsx
+++ about-section/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
             
             <div className="feature">ds
               <h3>Our Values</h3>ssss
-              <p>Innovation, s, and  in wssw ssssssssssssssssssssss</p>
+              <p>Innovation, s, and  in wssw ssssssssssssssssssssssss</p>
             </div>
           </div>
         </section>
